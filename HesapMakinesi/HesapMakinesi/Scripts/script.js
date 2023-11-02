72ca3c43dc3ce39ad9b5aa53a587872667da7134
SBEGENJOV <seydi19981995@gmail.com>
2023-11-02 15:16:40 +0300

    First Upload

---
+++ HesapMakinesi/HesapMakinesi/Scripts/script.js
@@ -0,0 +1,59 @@
+﻿//---------------------------------------------------------------- Hesaplama işlemlerini yapan kısım --------------------------------------------------------
+var islem;
+function islemSonuc() {
+    islem = document.getElementById("metin").value;
+    if (islem == isNaN) {
+        alert("hata işlemini gözden geçir");
+    }
+    else {
+        document.getElementById("metin").value = eval(islem);
+    }
+}
+//--------------------------------------------------- Buttonlara basıldıgında alana sayı ve işlemlerin gelmesini saglar. -------------------------------------------------
+function bir() {
+    var birr = document.getElementById("metin").value += 1;
+}
+function iki() {
+    document.getElementById("metin").value += 2;
+}
+function uc() {
+    document.getElementById("metin").value += 3;
+}
+function dort() {
+    document.getElementById("metin").value += 4;
+} function bes() {
+    document.getElementById("metin").value += 5;
+} function alti() {
+    document.getElementById("metin").value += 6;
+} function yedi() {
+    document.getElementById("metin").value += 7;
+} function sekiz() {
+    document.getElementById("metin").value += 8;
+} function dokuz() {
+    document.getElementById("metin").value += 9;
+}
+function sifir() {
+    document.getElementById("metin").value += 0;
+}
+function arti() {
+    document.getElementById("metin").value += "+";
+}
+function eksi() {
+    document.getElementById("metin").value += "-";
+} function carp() {
+    document.getElementById("metin").value += "*";
+} function bol() {
+    document.getElementById("metin").value += "/";
+}
+function temiz() {
+    document.getElementById("metin").value = "";
+}
+//------------------------------------------------------ Klavyeden harf ve özel karekter girmesini engeller.-----------------------------------------------------------
+function engelle() {
+    if (event.keyCode == 8 || (event.keyCode >= 96 && event.keyCode <= 111)) {
+        return;
+    }
+    event.preventDefault();
+}
+document.getElementById("metin").addEventListener("keydown", engelle);
+//-----------------------------------------------------------------------------------------------------------------------------------------
