7bdbdf5ec011a4e1cddd8d3a07664c053be3f82f
SBEGENJOV <seydi19981995@gmail.com>
2023-11-07 13:15:02 +0300

    07.11.2023

--- HesapMakinesi/HesapMakinesi/Scripts/script.js
+++ HesapMakinesi/HesapMakinesi/Scripts/script.js
@@ -1,3 +1,56 @@
-﻿//---------------------------------------------------------------- Hesaplama işlemlerini yapan kısım --------------------------------------------------------
+﻿        var islem;
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : window.event.keyCode;
+    if ((charCode > 105 || charCode < 112) && (charCode > 31 && charCode < 57))
+        return true;
+    return false;
+}
 
-//-----------------------------------------------------------------------------------------------------------------------------------------
+        function islemSonuc() {
+            islem = document.getElementById("metin").value;
+            if (islem == isNaN) {
+                alert("hata işlemini gözden geçir");
+            }
+            else {
+                document.getElementById("metin").value = eval(islem);
+            }
+        }
+        function bir() {
+            document.getElementById("metin").value += 1;
+        }
+        function iki() {
+            document.getElementById("metin").value += 2;
+        }
+        function uc() {
+            document.getElementById("metin").value += 3;
+        }
+        function dort() {
+            document.getElementById("metin").value += 4;
+        } function bes() {
+            document.getElementById("metin").value += 5;
+        } function alti() {
+            document.getElementById("metin").value += 6;
+        } function yedi() {
+            document.getElementById("metin").value += 7;
+        } function sekiz() {
+            document.getElementById("metin").value += 8;
+        } function dokuz() {
+            document.getElementById("metin").value += 9;
+        }
+        function sifir() {
+            document.getElementById("metin").value += 0;
+        }
+        function arti() {
+            document.getElementById("metin").value += "+";
+        }
+        function eksi() {
+            document.getElementById("metin").value += "-";
+        } function carp() {
+            document.getElementById("metin").value += "*";
+        } function bol() {
+            document.getElementById("metin").value += "/";
+        }
+        function temiz() {
+            document.getElementById("metin").value = "";
+        }
+        
